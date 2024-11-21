e183792f6f4d6a677fe72a6d66b402289ea1198c
chandan-pradhan-digiqc <chandan@digiqc.com>
2024-11-21 12:48:47 +0530

    fix: node versions fixed - #123

--- src/utils/cors-options.js
+++ src/utils/cors-options.js
@@ -1,27 +1,16 @@
-// const CONFIG = require('../config/config');
-
-// const corsOptionsDelegate = (req, callback) => {
-//   const allowlistOrigins = CONFIG.WEB_URLS && CONFIG.WEB_URLS.length > 0 ? CONFIG.WEB_URLS.split(',') : ['http://localhost:3000'];
-//   let corsOptions = {
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     exposedHeaders: ['Content-Type', 'Content-Length', 'Content-Disposition'],
-//   };
-//   if (allowlistOrigins.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { ...corsOptions, origin: true }; // reflect (enable) the requested origin in the CORS response
-//   } else {
-//     corsOptions = { origin: false }; // disable CORS for this request
-//   }
-//   callback(null, corsOptions); // callback expects two parameters: error and options
-// };
-
-// module.exports = corsOptionsDelegate;
+const CONFIG = require('../config/config');
 
 const corsOptionsDelegate = (req, callback) => {
-  const corsOptions = {
+  const allowlistOrigins = CONFIG.WEB_URLS && CONFIG.WEB_URLS.length > 0 ? CONFIG.WEB_URLS.split(',') : ['http://localhost:3000'];
+  let corsOptions = {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     exposedHeaders: ['Content-Type', 'Content-Length', 'Content-Disposition'],
-    origin: true, // Allow all origins
   };
+  if (allowlistOrigins.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { ...corsOptions, origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
   callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
